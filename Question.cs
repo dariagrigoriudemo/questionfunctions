fb433cb220fcec6e2a93ce5bb2bf06d1d884b5f4
Daria Grigoriu <dariac@ntdev.microsoft.com>
2020-01-08 14:04:19 -0800

    CosmosDB integration

--- Question.cs
+++ Question.cs
@@ -1,12 +1,15 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Collections.Generic;
+using Microsoft.Azure.Documents;
 
 namespace QuestionFunction
 {
@@ -14,38 +17,39 @@ namespace QuestionFunction
     {
         private const string FunctionQuestionSignal = "Meaning";
 
-        private static readonly string[] GenericFactAboutAzureFunctions = 
-            new string[] {
-                "Serverless technologies may not be the meaning of life, but they can make your life more meaningful and more productive!",
-                "With great functions comes great productivity!",
-                "Serverless is so 2020!",
-                "To be or not to be serverless!"
-        };
-
-        [FunctionName("Answer")]
+        [FunctionName("Question")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get","post", Route = null)] HttpRequest req,
+            [CosmosDB(
+                databaseName: "questionsdb",
+                collectionName: "questions",
+                ConnectionStringSetting = "questionsDbConnectionString",
+                SqlQuery = "SELECT * FROM questions"
+            )] IEnumerable<QuestionDetail> documents,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             string question = req.Query["question"];
 
+            log.LogInformation(documents.Count().ToString());
+
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             question = question ?? data?.question;
 
             return question != null
-                ? (ActionResult)new OkObjectResult(getAnswer(question))
+                ? (ActionResult)new OkObjectResult(getAnswer(question, documents))
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
 
-        private static string getAnswer(string question) {
+        private static string getAnswer(string question, IEnumerable<QuestionDetail> documents) {
 
             if (question.Contains(FunctionQuestionSignal, StringComparison.InvariantCultureIgnoreCase))
             {
                 Random rnd = new Random();
-                return GenericFactAboutAzureFunctions[rnd.Next(GenericFactAboutAzureFunctions.Length)];
+                var allDocuments = documents.ToArray();
+                return allDocuments[rnd.Next(allDocuments.Length)].answer_text;
             }
             else {
                 throw new NotImplementedException("Generic question answering not yet supported");
---
+++ QuestionDetail.cs
@@ -0,0 +1,9 @@
+﻿namespace QuestionFunction
+{
+    public class QuestionDetail
+    {
+        public string id { get; set; } 
+        public string question_topic { get; set; }
+        public string answer_text { get; set; }
+    }
+}
